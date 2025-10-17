c1d730993a1a4ea7446dad20125c9d1466007442
Tony Thompson <tony.thompson@ontario.ca>
2025-10-17 16:26:29 -0400

    Added manifoldCAD import.  Experimental.

--- rollup.config.js
+++ rollup.config.js
@@ -8,6 +8,10 @@ export default {
         { file: pkg.main, format: 'cjs' },
         { file: pkg.module, format: 'es' }
     ],
+    external: [
+        'manifold-3d',
+        'manifold-3d/manifoldCAD'
+    ],
     plugins: [
         nodeResolve(), // Find packages in node_modules.
         commonjs(), // Convert any CJS dependencies to ESM.
