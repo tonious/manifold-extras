60caeef04ac0c2c4e8a9ad514ea1f07109224d12
Tony Thompson <tony.thompson@ontario.ca>
2025-10-18 14:11:41 -0400

    Add cylinderWarp

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export {polarWarp} from "./lib/polarwarp.mjs"
+export {polarWarp, cylinderWarp} from "./lib/polarwarp.mjs"
 export {text} from "./lib/hershey.mjs"
 export {axes} from "./lib/axes.mjs"
 export * as measurements from "./lib/measurements.mjs"
